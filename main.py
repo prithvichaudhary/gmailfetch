5ba6b9cd9dfa072ba32a31576d2c31bd711e9d0b
Prithvi Chaudhary <104778560+prithvichaudhary@users.noreply.github.com>
2024-07-03 19:56:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,46 @@ def get_emails(result_bytes):
 		msgs.append(data)
 
 	return msgs
-
+con = imaplib.IMAP4_SSL(imap_url) 
+ 
+# logging the user in
+con.login(user, password) 
+ 
+# calling function to check for email under this label
+con.select('Inbox') 
+ 
+ # fetching emails from this user "tu**h*****1@gmail.com"
+msgs = get_emails(search('FROM', 'MY_ANOTHER_GMAIL_ADDRESS', con))
+ 
+# Uncomment this to see what actually comes as data 
+# print(msgs) 
+ 
+ 
+# Finding the required content from our msgs
+# User can make custom changes in this part to
+# fetch the required content he / she needs
+ 
+# printing them by the order they are displayed in your gmail 
+for msg in msgs[::-1]: 
+    for sent in msg:
+        if type(sent) is tuple: 
+ 
+            # encoding set as utf-8
+            content = str(sent[1], 'utf-8') 
+            data = str(content)
+ 
+            # Handling errors related to unicodenecode
+            try: 
+                indexstart = data.find("ltr")
+                data2 = data[indexstart + 5: len(data)]
+                indexend = data2.find("</div>")
+ 
+                # printing the required content which we need
+                # to extract from our email i.e our body
+                print(data2[0: indexend])
+ 
+            except UnicodeEncodeError as e:
+                pass
 
 	
 		
