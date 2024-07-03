e2b8238e644178dd807b283dfabd100749aa1069
Prithvi Chaudhary <104778560+prithvichaudhary@users.noreply.github.com>
2024-07-03 19:55:41 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,13 @@
+# Importing libraries
+import imaplib, email
+
+user = 'USER_EMAIL_ADDRESS'
+password = 'USER_PASSWORD'
+imap_url = 'imap.gmail.com'
+
+# Function to get email content part i.e its body part
+
+
+
+	
+		
